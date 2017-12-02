5526ff5339ab4f10f3e4d3514d3eb360e33ef46b
Isaias Valle <guitartsword@gmail.com>
2017-12-01 19:37:38 -0600

    implemented vis in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import {FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import vis from 'vis';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    // create an array with nodes
+    const nodesArray = [
+    ];
+    // create an array with edges
+    const edgesArray = [
+    ];
+    //Create a network
+    this.data = {
+      nodes: new vis.DataSet(nodesArray),
+      edges: new vis.DataSet(edgesArray)
+    };
+    this.options = {};
+    this.startNetwork = this.startNetwork.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = ({
+      city:''
+    });
+    this.cityList = [];
+  }
   render() {
     return (
       <div className="App">
@@ -10,11 +34,64 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FormGroup>
+            <ControlLabel>Nombre de la ciudad</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="Tegucigalpa"
+              value={this.state.city}
+              name='city'
+              onChange={this.handleInputChange}/>
+          </FormGroup>
+          <Button
+            bsStyle="primary"
+            name="addCity"
+            onClick={this.handleClick}
+          >
+            AgregarCiudad
+          </Button>
+        <div id="network"></div>
+        
       </div>
     );
+  }
+  componentDidMount() {
+    this.network = this.startNetwork(document.getElementById('network'));
+  }
+  handleClick(event){
+    if(!this.state.city.trim()){
+      alert('La ciudad tiene que tener un nombre');
+      return
+    }
+    try {
+      this.data.nodes.add({
+        id:this.state.city,
+        label:this.state.city
+      });
+      this.data.nodes.forEach((city)=>{
+        if(city.id !== this.state.city)
+          this.data.edges.add({
+            from:this.state.city,
+            to: city.id,
+            label: Math.ceil(Math.random()*10) + 'km',
+            font: {align:'horizontal'}
+          });
+      })
+      this.cityList.shift(this.state.city);
+    } catch (error) {
+      alert(`Ciudad con nombre ${this.state.city} no se puede agregar`);
+    }
+  }
+  handleInputChange(event){
+		this.setState({
+			[event.target.name]:event.target.value
+		});
+	}
+  startNetwork(container) {
+    let data = this.data;
+    let options = this.options;
+    let network = new vis.Network(container, data, options);
+    return network;
   }
 }
 
