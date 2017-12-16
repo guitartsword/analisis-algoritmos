83d7ed2e3cc76d8b920e6cfd5d3abc70f814c9d7
Isaias Valle <guitartsword@gmail.com>
2017-12-15 20:52:38 -0600

    finished k-coloring graph

--- src/GraphColoring/GraphColoring.js
+++ src/GraphColoring/GraphColoring.js
@@ -3,7 +3,22 @@ import { Button } from 'react-bootstrap';
 import vis from 'vis';
 import VisNetwork from '../VisNetwork/VisNetwork';
 import swal from 'sweetalert';
-
+const COLORS = {
+	1: 'green',
+	2: 'orange',
+	4: 'red',
+	8: 'yellow',
+	16: 'magenta',
+	32: 'cyan',
+	64: 'blue',
+	128: 'gray',
+	256: 'gold',
+	512: 'brown',
+	1024: 'purple',
+	2048: 'palevioletred',
+	4096: 'crimson',
+	8192: 'rosybrown'
+};
 class GraphColoring extends Component {
 	constructor(props){
 		super(props);
@@ -24,8 +39,8 @@ class GraphColoring extends Component {
 				barnesHut: {
 					gravitationalConstant: -7000,
 					centralGravity: 0.1,
-					springLength: 100,
-					springConstant: 0.07,
+					springLength: 300,
+					springConstant: 0.02,
 					avoidOverlap: 0.18
 				},
 				minVelocity: 0.75
@@ -44,6 +59,13 @@ class GraphColoring extends Component {
 					onClick={this.handleClick}
 				>
 						Agregar Nodos
+				</Button>
+				<Button
+					bsStyle="primary"
+					name="addInterconectedNodes"
+					onClick={this.handleClick}
+				>
+						Agregar Nodos Interconectados
 				</Button>
 				<VisNetwork data={this.data} options={this.options}/>
 				<Button
@@ -75,8 +97,61 @@ class GraphColoring extends Component {
 					//this.data.network.stabilize();
 				});
 			},
+			addInterconectedNodes: () => {
+				swal('Ingrese la cantidad de nodos a agregar',{
+					content: 'input'
+				}).then((cantidad)=>{
+					cantidad = parseInt(cantidad, 10) || 0;
+					if(!cantidad){
+						swal('No se agregaron nodos', 'Ingrese un numero valido', 'info');
+						return;
+					}
+					this.data.nodes.clear();
+					this.data.edges.clear();
+
+					let newData = VisNetwork.createCompleteGraph(cantidad);
+					this.data.nodes.add(newData.nodes);
+					this.data.edges.add(newData.edges);
+					//this.data.network.stabilize();
+				});
+			},
 			calculateDSatur: () => {
-				swal('In progress', 'wait until finish', 'info');
+				let nodeList = this.data.nodes.getIds();
+				let network = this.data.network;
+
+				nodeList.sort((a,b)=>{
+					const alength = network.getConnectedNodes(a).length;
+					const blength = network.getConnectedNodes(b).length;
+					return blength - alength;
+				});
+				let totalColors = 0;
+				const startTime = performance.now();
+				nodeList.forEach((node)=>{
+					let connectedNodes  = network.getConnectedNodes(node);
+					let colorsValue = 0;
+					connectedNodes.forEach((connected) => {
+						let nodeData = this.data.nodes.get(connected);
+						if(nodeData.value !== undefined){
+							colorsValue |= nodeData.value;
+						}
+					});
+					let i = 0;
+					do{
+						let updateNode = this.data.nodes.get(node);
+						updateNode.color = {background: COLORS[1 << i]};
+						updateNode.value = 1 << i;
+						if(i > totalColors){
+							totalColors = i;
+						}
+						this.data.nodes.update(updateNode);
+					}while(colorsValue & (1 << i++));
+				});
+				const endTime = performance.now();
+				swal(
+					`Cantidad de colores minimo encontrado es: ${totalColors + 1}`,
+					`Se tardo ${endTime -startTime} milisegundos en realizar`,
+					'info'
+				);
 			}
 		};
 		clickEvents[event.target.name]();
